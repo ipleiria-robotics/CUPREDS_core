3ce1c8719b8b7aee0378e985f3b48e2361b0416d
carlostojal <carlos.tojal@hotmail.com>
2023-09-21 19:15:47 +0100

    collapse the namespaces on the StampedPointCloud

--- include/pcl_aggregator_core/entities/StampedPointCloud.h
+++ include/pcl_aggregator_core/entities/StampedPointCloud.h
@@ -15,110 +15,108 @@
 
 #define POINTCLOUD_ORIGIN_NONE "none"
 
-namespace pcl_aggregator {
-    namespace entities {
+namespace pcl_aggregator::entities {
 
-        /*! \brief Stamped Point Cloud
-         *         A PointCloud with an associated timestamp. Also has other utilities.
-         */
-        class StampedPointCloud {
+    /*! \brief Stamped Point Cloud
+     *         A PointCloud with an associated timestamp. Also has other utilities.
+     */
+    class StampedPointCloud {
 
-            private:
-                /*! \brief The timestamp */
-                unsigned long long timestamp;
+        private:
+            /*! \brief The timestamp */
+            unsigned long long timestamp;
 
-                /*! \brief The PointCloud */
-                typename pcl::PointCloud<pcl::PointXYZRGBL>::Ptr cloud = nullptr;
+            /*! \brief The PointCloud */
+            typename pcl::PointCloud<pcl::PointXYZRGBL>::Ptr cloud = nullptr;
 
-                /*! \brief Was the transform to the robot frame computed? */
-                bool transformComputed = false;
+            /*! \brief Was the transform to the robot frame computed? */
+            bool transformComputed = false;
 
-                /*! \brief Was the transform computed by ICP applied? */
-                bool icpTransformComputed = false;
+            /*! \brief Was the transform computed by ICP applied? */
+            bool icpTransformComputed = false;
 
-                // the name of the topic the pointcloud came from
-                std::string originTopic = POINTCLOUD_ORIGIN_NONE;
+            // the name of the topic the pointcloud came from
+            std::string originTopic = POINTCLOUD_ORIGIN_NONE;
 
-                /*! \brief Label used to identify each PointCloud, for example on removal */
-                std::uint32_t label;
+            /*! \brief Label used to identify each PointCloud, for example on removal */
+            std::uint32_t label;
 
-                /*! \brief Mutex to contain access to this PointCloud. */
-                std::mutex cloudMutex;
+            /*! \brief Mutex to contain access to this PointCloud. */
+            std::mutex cloudMutex;
 
-                /*! \brief Generate a label to the PointCloud based on the origin topic name and timestamp. */
-                std::uint32_t generateLabel();
+            /*! \brief Generate a label to the PointCloud based on the origin topic name and timestamp. */
+            std::uint32_t generateLabel();
 
-            public:
-                StampedPointCloud(std::string originTopic);
-                ~StampedPointCloud();
+        public:
+            StampedPointCloud(const std::string& originTopic);
+            ~StampedPointCloud();
 
-                /*! \brief Get the PointCloud timestamp. */
-                unsigned long long getTimestamp() const;
-                /*! \brief Get a smart pointer to the PointCloud. */
-                typename pcl::PointCloud<pcl::PointXYZRGBL>::Ptr& getPointCloud();
-                /*! \brief Get the origin topic name. */
-                std::string getOriginTopic() const;
-                /*! \brief Get the label of the PointCloud. Should be unique. */
-                std::uint32_t getLabel() const;
-                /*! \brief Set the PointCloud's timestamp. */
-                void setTimestamp(unsigned long long t);
-                /*! \brief Set the PointCloud by smart pointer. This method moves the pointer, does not copy it or increment use count.
-                 * @param cloud The PointCloud smart pointer to move from.
-                 * @param assignGeneratedLabel Assign a generated label or not. Generating the label has an additional overhead, but is usually needed.
-                 * */
-                void setPointCloud(typename pcl::PointCloud<pcl::PointXYZRGBL>::Ptr cloud, bool assignGeneratedLabel=true);
-                /*! \brief Set the origin topic name.
-                 * @param origin The topic name.
-                 */
-                void setOriginTopic(const std::string& origin);
+            /*! \brief Get the PointCloud timestamp. */
+            unsigned long long getTimestamp() const;
+            /*! \brief Get a smart pointer to the PointCloud. */
+            typename pcl::PointCloud<pcl::PointXYZRGBL>::Ptr& getPointCloud();
+            /*! \brief Get the origin topic name. */
+            std::string getOriginTopic() const;
+            /*! \brief Get the label of the PointCloud. Should be unique. */
+            std::uint32_t getLabel() const;
+            /*! \brief Set the PointCloud's timestamp. */
+            void setTimestamp(unsigned long long t);
+            /*! \brief Set the PointCloud by smart pointer. This method moves the pointer, does not copy it or increment use count.
+             * @param cloud The PointCloud smart pointer to move from.
+             * @param assignGeneratedLabel Assign a generated label or not. Generating the label has an additional overhead, but is usually needed.
+             * */
+            void setPointCloud(typename pcl::PointCloud<pcl::PointXYZRGBL>::Ptr cloud, bool assignGeneratedLabel=true);
+            /*! \brief Set the origin topic name.
+             * @param origin The topic name.
+             */
+            void setOriginTopic(const std::string& origin);
 
-                /*! \brief Check if the transform to the robot base frame was computed. */
-                bool isTransformComputed() const;
-                /*! \brief Apply the robot frame transform. */
-                void applyTransform(const Eigen::Affine3d& tf);
+            /*! \brief Check if the transform to the robot base frame was computed. */
+            bool isTransformComputed() const;
+            /*! \brief Apply the robot frame transform. */
+            void applyTransform(const Eigen::Affine3d& tf);
 
-                /*! \brief Check if the ICP transform was computed on this PointCloud. */
-                bool isIcpTransformComputed() const;
-                /*! \brief Apply the ICP transform. */
-                void applyIcpTransform(const Eigen::Matrix4f& tf);
+            /*! \brief Check if the ICP transform was computed on this PointCloud. */
+            bool isIcpTransformComputed() const;
+            /*! \brief Apply the ICP transform. */
+            void applyIcpTransform(const Eigen::Matrix4f& tf);
 
-                /*! \brief Assign a label to a PointCloud.
-                 *
-                 * @param cloud The PointCloud's smart pointer.
-                 * @param label The 32-bit unsigned label to assign.
-                 */
-                static void assignLabelToPointCloud(const typename pcl::PointCloud<pcl::PointXYZRGBL>::Ptr& cloud, std::uint32_t label);
+            /*! \brief Assign a label to a PointCloud.
+             *
+             * @param cloud The PointCloud's smart pointer.
+             * @param label The 32-bit unsigned label to assign.
+             */
+            static void assignLabelToPointCloud(const typename pcl::PointCloud<pcl::PointXYZRGBL>::Ptr& cloud, std::uint32_t label);
 
-                /*! \brief Remove points with a given label from the current PointCloud.
-                 *
-                 * @param label The label to remove.
-                 */
-                void removePointsWithLabel(std::uint32_t label);
+            /*! \brief Remove points with a given label from the current PointCloud.
+             *
+             * @param label The label to remove.
+             */
+            void removePointsWithLabel(std::uint32_t label);
 
-                /*! \brief Remove points with the given labels from the current PointCloud.
-                 *
-                 * @param labels The labels to remove.
-                 */
-                void removePointsWithLabels(std::set<std::uint32_t> labels);
+            /*! \brief Remove points with the given labels from the current PointCloud.
+             *
+             * @param labels The labels to remove.
+             */
+            void removePointsWithLabels(std::set<std::uint32_t> labels);
 
-                /*! \brief Apply voxel grid filter to the PointCloud.
-                 *
-                 * @param leafSize
-                 */
-                void downsample(float leafSize);
+            /*! \brief Apply voxel grid filter to the PointCloud.
+             *
+             * @param leafSize
+             */
+            void downsample(float leafSize);
 
-        };
+    };
 
-        /*! \brief Custom comparison functor between stamped point clouds. The comparison criteria is the timestamp. */
-        struct CompareStampedPointCloudPointers {
+    /*! \brief Custom comparison functor between stamped point clouds. The comparison criteria is the timestamp. */
+    struct CompareStampedPointCloudPointers {
 
-            bool operator()(const std::shared_ptr<StampedPointCloud>& first,
-                    const std::shared_ptr<StampedPointCloud>& second) const {
-                return first->getTimestamp() < second->getTimestamp();
-            }
-        };
+        bool operator()(const std::shared_ptr<StampedPointCloud>& first,
+                const std::shared_ptr<StampedPointCloud>& second) const {
+            return first->getTimestamp() < second->getTimestamp();
+        }
+    };
 
-    } // pcl_aggregator
-} // entities
+} // pcl_aggregator::entities
 
 #endif //PCL_AGGREGATOR_CORE_STAMPEDPOINTCLOUD_H
--- src/entities/StampedPointCloud.cpp
+++ src/entities/StampedPointCloud.cpp
@@ -7,161 +7,160 @@
 #include <pcl_aggregator_core/cuda/CUDAPointClouds.cuh>
 #include <utility>
 
-namespace pcl_aggregator {
-    namespace entities {
 
-        StampedPointCloud::StampedPointCloud(std::string originTopic) {
-            this->timestamp = utils::Utils::getCurrentTimeMillis();
+namespace pcl_aggregator::entities {
 
-            this->setOriginTopic(originTopic);
+    StampedPointCloud::StampedPointCloud(const std::string& originTopic) {
+        this->timestamp = utils::Utils::getCurrentTimeMillis();
 
-            this->label = generateLabel();
+        this->setOriginTopic(originTopic);
 
-            this->cloud = pcl::PointCloud<pcl::PointXYZRGBL>::Ptr(new pcl::PointCloud<pcl::PointXYZRGBL>());
+        this->label = generateLabel();
+
+        this->cloud = pcl::PointCloud<pcl::PointXYZRGBL>::Ptr(new pcl::PointCloud<pcl::PointXYZRGBL>());
+    }
+
+    StampedPointCloud::~StampedPointCloud() {
+
+        std::lock_guard<std::mutex> lock(cloudMutex);
+        // the StampedPointCloud owns its cloud's pointer and should destroy it
+        this->cloud.reset();
+    }
+
+    // generate a 32-bit label and assign
+    std::uint32_t StampedPointCloud::generateLabel() {
+
+        std::string combined = this->originTopic + std::to_string(this->timestamp);
+
+        std::hash<std::string> hasher;
+        std::uint32_t hash_value = hasher(combined);
+
+        return hash_value;
+    }
+
+    unsigned long long StampedPointCloud::getTimestamp() const {
+        return this->timestamp;
+    }
+
+    typename pcl::PointCloud<pcl::PointXYZRGBL>::Ptr& StampedPointCloud::getPointCloud() {
+        std::lock_guard<std::mutex> lock(cloudMutex);
+        return cloud;
+    }
+
+    std::string StampedPointCloud::getOriginTopic() const {
+        return this->originTopic;
+    }
+
+    std::uint32_t StampedPointCloud::getLabel() const {
+        return this->label;
+    }
+
+    bool StampedPointCloud::isIcpTransformComputed() const {
+        return icpTransformComputed;
+    }
+
+    void StampedPointCloud::setTimestamp(unsigned long long t) {
+        this->timestamp = t;
+    }
+
+    void StampedPointCloud::setPointCloud(typename pcl::PointCloud<pcl::PointXYZRGBL>::Ptr c, bool assignGeneratedLabel) {
+
+        std::lock_guard<std::mutex> lock(cloudMutex);
+
+        // free the old pointcloud
+        this->cloud.reset();
+
+        // set the new
+        this->cloud = std::move(c);
+
+        if(this->cloud != nullptr) {
+            if (assignGeneratedLabel)
+                StampedPointCloud::assignLabelToPointCloud(this->cloud, this->label);
+        } else {
+            std::cerr << "StampedPointCloud::setPointCloud: cloud is null!" << std::endl;
+        }
+    }
+
+    void StampedPointCloud::assignLabelToPointCloud(const typename pcl::PointCloud<pcl::PointXYZRGBL>::Ptr& cloud, std::uint32_t label) {
+
+        if(cloud != nullptr) {
+            cuda::pointclouds::setPointCloudLabelCuda(cloud, label);
+        } else {
+            std::cerr << "StampedPointCloud::assignLabelToPointCloud: cloud is null!" << std::endl;
+        }
+    }
+
+    void StampedPointCloud::setOriginTopic(const std::string& origin) {
+        this->originTopic = origin;
+    }
+
+    bool StampedPointCloud::isTransformComputed() const {
+        return this->transformComputed;
+    }
+
+    void StampedPointCloud::applyTransform(const Eigen::Affine3d& tf) {
+
+        std::lock_guard<std::mutex> lock(cloudMutex);
+
+        if(this->cloud != nullptr) {
+
+            // call a CUDA thread to transform the pointcloud in-place
+            cuda::pointclouds::transformPointCloudCuda(this->cloud, tf);
+        } else {
+            std::cerr << "StampedPointCloud::applyTransform: cloud is null!" << std::endl;
         }
 
-        StampedPointCloud::~StampedPointCloud() {
+        // pcl::transformPointCloud(*this->cloud, *this->cloud, tf);
+        this->transformComputed = true;
+    }
 
-            std::lock_guard<std::mutex> lock(cloudMutex);
-            // the StampedPointCloud owns its cloud's pointer and should destroy it
-            this->cloud.reset();
+    void StampedPointCloud::applyIcpTransform(const Eigen::Matrix4f& tf) {
+
+        if(!icpTransformComputed) {
+
+            Eigen::Matrix4d mat4d = tf.cast<double>();
+            Eigen::Affine3d affine(mat4d);
+
+            this->applyTransform(affine);
+
+            this->icpTransformComputed = true;
         }
+    }
 
-        // generate a 32-bit label and assign
-        std::uint32_t StampedPointCloud::generateLabel() {
+    void StampedPointCloud::removePointsWithLabel(std::uint32_t label) {
 
-            std::string combined = this->originTopic + std::to_string(this->timestamp);
+        std::lock_guard<std::mutex> lock(this->cloudMutex);
 
-            std::hash<std::string> hasher;
-            std::uint32_t hash_value = hasher(combined);
-
-            return hash_value;
+        auto it = this->cloud->begin();
+        while (it != this->cloud->end()) {
+            if (it->label == label)
+                it = this->cloud->erase(it);
+            else
+                ++it;
         }
+    }
 
-        unsigned long long StampedPointCloud::getTimestamp() const {
-            return this->timestamp;
+    void StampedPointCloud::removePointsWithLabels(std::set<std::uint32_t> labels) {
+
+        std::lock_guard<std::mutex> lock(this->cloudMutex);
+
+        auto it = this->cloud->begin();
+        while (it != this->cloud->end()) {
+            // if the label is in the set, remove it
+            if (labels.find(it->label) != labels.end())
+                it = this->cloud->erase(it);
+            else
+                ++it;
         }
+    }
 
-        typename pcl::PointCloud<pcl::PointXYZRGBL>::Ptr& StampedPointCloud::getPointCloud() {
-            std::lock_guard<std::mutex> lock(cloudMutex);
-            return cloud;
-        }
+    void StampedPointCloud::downsample(float leafSize) {
 
-        std::string StampedPointCloud::getOriginTopic() const {
-            return this->originTopic;
-        }
+        std::lock_guard<std::mutex> lock(this->cloudMutex);
 
-        std::uint32_t StampedPointCloud::getLabel() const {
-            return this->label;
-        }
-
-        bool StampedPointCloud::isIcpTransformComputed() const {
-            return icpTransformComputed;
-        }
-
-        void StampedPointCloud::setTimestamp(unsigned long long t) {
-            this->timestamp = t;
-        }
-
-        void StampedPointCloud::setPointCloud(typename pcl::PointCloud<pcl::PointXYZRGBL>::Ptr c, bool assignGeneratedLabel) {
-
-            std::lock_guard<std::mutex> lock(cloudMutex);
-
-            // free the old pointcloud
-            this->cloud.reset();
-
-            // set the new
-            this->cloud = std::move(c);
-
-            if(this->cloud != nullptr) {
-                if (assignGeneratedLabel)
-                    StampedPointCloud::assignLabelToPointCloud(this->cloud, this->label);
-            } else {
-                std::cerr << "StampedPointCloud::setPointCloud: cloud is null!" << std::endl;
-            }
-        }
-
-        void StampedPointCloud::assignLabelToPointCloud(const typename pcl::PointCloud<pcl::PointXYZRGBL>::Ptr& cloud, std::uint32_t label) {
-
-            if(cloud != nullptr) {
-                cuda::pointclouds::setPointCloudLabelCuda(cloud, label);
-            } else {
-                std::cerr << "StampedPointCloud::assignLabelToPointCloud: cloud is null!" << std::endl;
-            }
-        }
-
-        void StampedPointCloud::setOriginTopic(const std::string& origin) {
-            this->originTopic = origin;
-        }
-
-        bool StampedPointCloud::isTransformComputed() const {
-            return this->transformComputed;
-        }
-
-        void StampedPointCloud::applyTransform(const Eigen::Affine3d& tf) {
-
-            std::lock_guard<std::mutex> lock(cloudMutex);
-
-            if(this->cloud != nullptr) {
-
-                // call a CUDA thread to transform the pointcloud in-place
-                cuda::pointclouds::transformPointCloudCuda(this->cloud, tf);
-            } else {
-                std::cerr << "StampedPointCloud::applyTransform: cloud is null!" << std::endl;
-            }
-
-            // pcl::transformPointCloud(*this->cloud, *this->cloud, tf);
-            this->transformComputed = true;
-        }
-
-        void StampedPointCloud::applyIcpTransform(const Eigen::Matrix4f& tf) {
-
-            if(!icpTransformComputed) {
-
-                Eigen::Matrix4d mat4d = tf.cast<double>();
-                Eigen::Affine3d affine(mat4d);
-
-                this->applyTransform(affine);
-
-                this->icpTransformComputed = true;
-            }
-        }
-
-        void StampedPointCloud::removePointsWithLabel(std::uint32_t label) {
-
-            std::lock_guard<std::mutex> lock(this->cloudMutex);
-
-            auto it = this->cloud->begin();
-            while (it != this->cloud->end()) {
-                if (it->label == label)
-                    it = this->cloud->erase(it);
-                else
-                    ++it;
-            }
-        }
-
-        void StampedPointCloud::removePointsWithLabels(std::set<std::uint32_t> labels) {
-
-            std::lock_guard<std::mutex> lock(this->cloudMutex);
-
-            auto it = this->cloud->begin();
-            while (it != this->cloud->end()) {
-                // if the label is in the set, remove it
-                if (labels.find(it->label) != labels.end())
-                    it = this->cloud->erase(it);
-                else
-                    ++it;
-            }
-        }
-
-        void StampedPointCloud::downsample(float leafSize) {
-
-            std::lock_guard<std::mutex> lock(this->cloudMutex);
-
-            pcl::VoxelGrid<pcl::PointXYZRGBL> voxelGrid;
-            voxelGrid.setInputCloud(this->cloud);
-            voxelGrid.setLeafSize(leafSize, leafSize, leafSize);
-            voxelGrid.filter(*this->cloud);
-        }
-    } // pcl_aggregator
-} // entities
+        pcl::VoxelGrid<pcl::PointXYZRGBL> voxelGrid;
+        voxelGrid.setInputCloud(this->cloud);
+        voxelGrid.setLeafSize(leafSize, leafSize, leafSize);
+        voxelGrid.filter(*this->cloud);
+    }
+} // pcl_aggregator::entities
